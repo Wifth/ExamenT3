3b1397c2178f2471c4698bc54d542633ba4dd8b7
Wifth <alexander.obando.os2206@gmail.com>
2024-11-14 08:35:53 -0500

    Clases y Program

--- ExamenT3/Program.cs
+++ ExamenT3/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ExamenT3;
+
+byte opcion;
+string regresar;
+
+
+do
+{
+    Console.WriteLine("BIENVENIDOS AL SISTEMA DE REGISTRO DE EDADES \n");
+    Console.WriteLine("************ MENÚ *************");
+    Console.WriteLine("     * 1. Crear      *");
+    Console.WriteLine("     * 2. Lista      *");
+    Console.WriteLine("     * 3. Modificar  *");
+    Console.WriteLine("     * 0. Salir      *");
+    Console.WriteLine("*******************************\n");
+
+    Console.Write("Ingrese opción: ");
+
+    while (!byte.TryParse(Console.ReadLine(), out opcion) || opcion > 4)
+    {
+        Console.Write("Error: Ingrese opción: ");
+    }
+
+    switch (opcion)
+    {
+        case 0: Environment.Exit(0); break;
+        case 1: Crear(); break;
+        case 2: lista(); break;
+        case 3: Modificar(); break;
+    }
+    Console.Write("\nDesea regresar al menú? [si]: ");
+    regresar = Console.ReadLine().ToLower();
+    Console.Clear();
+} while (regresar == "si");
