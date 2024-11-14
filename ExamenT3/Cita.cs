b15205592d707195098f0bb23fba6c302a9999dd
Wifth <alexander.obando.os2206@gmail.com>
2024-11-14 08:23:35 -0500

    Clases

---
+++ ExamenT3/Cita.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenT3
+{
+    internal class Cita
+    {
+        public int Numero { get; set; }
+        public string Estudiante { get; set; }
+        public string Enfermedad { get; set; }
+        public double Precio { get; set; }
+        public void ToString()
+        {
+
+        }
+    }
+}
---
+++ ExamenT3/Estudiante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenT3
+{
+    internal class Estudiante
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Universidad { get; set; }
+        public void ToString()
+        {
+
+        }
+    }
+}
