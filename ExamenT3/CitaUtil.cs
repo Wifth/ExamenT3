34dbc67464a9e1b97ce611532733cc1c060cd198
Wifth <alexander.obando.os2206@gmail.com>
2024-11-14 08:42:34 -0500

    CitaUtil

---
+++ ExamenT3/CitaUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenT3
+{
+    internal class CitaUtil
+    {
+        public static void Crear()
+        {
+
+        }
+        public static void Lista()
+        {
+
+        }
+        public static void Modificar()
+        {
+
+        }
+    }
+}
